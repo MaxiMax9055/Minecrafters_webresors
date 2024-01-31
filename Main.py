a8dfd754974ef2f4f91614493bd037232ce25d13
MaxiMax9055 <151159289+MaxiMax9055@users.noreply.github.com>
2024-01-31 21:21:24 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('singup')
+def index():
+    return render_template('singup.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
